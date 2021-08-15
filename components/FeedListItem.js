b8bb876d4bd80132d84d1cc87445346c95dbc0d0
velopert <public.velopert@gmail.com>
2021-08-15 17:16:26 +0900

    7.1 Open WriteScreen on FeedListItem Press

--- components/FeedListItem.js
+++ components/FeedListItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {Platform, Pressable, StyleSheet, Text} from 'react-native';
-import {format, formatDistanceToNow} from 'date-fns';
+import {formatDistanceToNow, format} from 'date-fns';
 import {ko} from 'date-fns/locale';
+import {useNavigation} from '@react-navigation/native';
 
 function formatDate(date) {
   const d = new Date(date);
@@ -28,6 +29,13 @@ function truncate(text) {
 
 function FeedListItem({log}) {
   const {title, body, date} = log; // 사용하기 편하게 객체 구조 분해 할당
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    navigation.navigate('Write', {
+      log,
+    });
+  };
 
   return (
     <Pressable
@@ -35,7 +43,8 @@ function FeedListItem({log}) {
         styles.block,
         Platform.OS === 'ios' && pressed && {backgroundColor: '#efefef'},
       ]}
-      android_ripple={{color: '#ededed'}}>
+      android_ripple={{color: '#ededed'}}
+      onPress={onPress}>
       <Text style={styles.date}>{formatDate(date)}</Text>
       <Text style={styles.title}>{title}</Text>
       <Text style={styles.body}>{truncate(body)}</Text>
--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -6,9 +6,12 @@ import WriteEditor from '../components/WriteEditor';
 import WriteHeader from '../components/WriteHeader';
 import LogContext from '../contexts/LogContext';
 
-function WriteScreen() {
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+function WriteScreen({route}) {
+  const log = route.params?.log;
+
+  const [title, setTitle] = useState(log?.title ?? '');
+  const [body, setBody] = useState(log?.body ?? '');
+
   const navigation = useNavigation();
 
   const {onCreate} = useContext(LogContext);
