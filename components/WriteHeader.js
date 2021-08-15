09a605a6a3f0026272f43abd50968c5823175a41
velopert <public.velopert@gmail.com>
2021-08-15 17:42:55 +0900

    7.6 Change time

--- components/WriteHeader.js
+++ components/WriteHeader.js
@@ -1,13 +1,39 @@
 import {useNavigation} from '@react-navigation/native';
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {format} from 'date-fns';
+import {ko} from 'date-fns/locale';
+import React, {useState} from 'react';
+import {Pressable, StyleSheet, Text, View} from 'react-native';
 import TransparentCircleButton from './TransparentCircleButton';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
-function WriteHeader({onSave, onAskRemove, isEditing}) {
+function WriteHeader({onSave, onAskRemove, isEditing, date, onChangeDate}) {
   const navigation = useNavigation();
   const onGoBack = () => {
     navigation.pop();
   };
+
+  const [mode, setMode] = useState('date');
+  const [visible, setVisible] = useState(false);
+
+  const onPressDate = () => {
+    setMode('date');
+    setVisible(true);
+  };
+
+  const onPressTime = () => {
+    setMode('time');
+    setVisible(true);
+  };
+
+  const onConfirm = selectedDate => {
+    setVisible(false);
+    onChangeDate(selectedDate);
+  };
+
+  const onCancel = () => {
+    setVisible(false);
+  };
+
   return (
     <View style={styles.block}>
       <View style={styles.iconButtonWrapper}>
@@ -32,6 +58,26 @@ function WriteHeader({onSave, onAskRemove, isEditing}) {
           onPress={onSave}
         />
       </View>
+      <View style={styles.center}>
+        <Pressable onPress={onPressDate}>
+          <Text>
+            {format(new Date(date), 'PPP', {
+              locale: ko,
+            })}
+          </Text>
+        </Pressable>
+        <View style={styles.separator} />
+        <Pressable onPress={onPressTime}>
+          <Text>{format(new Date(date), 'p', {locale: ko})}</Text>
+        </Pressable>
+      </View>
+      <DateTimePickerModal
+        isVisible={visible}
+        mode={mode}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+        date={date}
+      />
     </View>
   );
 }
@@ -47,6 +93,20 @@ const styles = StyleSheet.create({
   buttons: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  center: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center',
+    zIndex: -1,
+    flexDirection: 'row',
+  },
+  separator: {
+    width: 8,
   },
 });
 
--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -12,6 +12,7 @@ function WriteScreen({route}) {
   const [title, setTitle] = useState(log?.title ?? '');
   const [body, setBody] = useState(log?.body ?? '');
   const navigation = useNavigation();
+  const [date, setDate] = useState(log ? new Date(log.date) : new Date());
 
   const {onCreate, onModify, onRemove} = useContext(LogContext);
 
@@ -40,7 +41,7 @@ function WriteScreen({route}) {
     if (log) {
       onModify({
         id: log.id,
-        date: log.date,
+        date: date.toISOString(),
         title,
         body,
       });
@@ -48,8 +49,7 @@ function WriteScreen({route}) {
       onCreate({
         title,
         body,
-        // 날짜를 문자열로 변환
-        date: new Date().toISOString(),
+        date: date.toISOString(),
       });
     }
     navigation.pop();
@@ -64,6 +64,8 @@ function WriteScreen({route}) {
           onSave={onSave}
           onAskRemove={onAskRemove}
           isEditing={!!log}
+          date={date}
+          onChangeDate={setDate}
         />
 
         <WriteEditor
